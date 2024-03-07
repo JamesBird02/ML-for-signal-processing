5e22dd160227984d09f60484cb70066bca727db9
James Bird <109037006+JamesBird02@users.noreply.github.com>
2024-03-07 11:40:48 +0000

    Update motion_sensor.py

--- motion_sensor.py
+++ motion_sensor.py
@@ -2,71 +2,60 @@ from picamera2 import Picamera2, Preview
 from gpiozero import MotionSensor
 from picamera2.encoders import H264Encoder
 from signal import pause
-from time import sleep
-import time
-import libcamera
-from datetime import datetime
 import threading
+from datetime import datetime
 
-# Global variables
 recording = False
-video_directory = '/home/pi/'
 motion_detected = False
+stop_recording_timer = None
+video_directory = '/home/james/'
 
-# Start video recording
-def start_recording():
-    global recording, motion_detected
-    if not recording:
-        print('recording')
-        current_datetime = datetime.now()
-        formated_date = current_datetime.strftime('%Y-%m-%d_%H-%M-%S')
-        video_filename = f'{video_directory}video_{formated_date}.h264'
-        camera.start_recording(encoder, video_filename) 
-        recording = True
-        motion_detected = True
-
-# stop video recording  
-def stop_recording():
-    global recording, motion_detected
-    if recording and not motion_detected:
-        print('No motion detected - Stopping recording in 5 seconds...')
-        #stop recording after 5 seconds if no motion is detected
-        threading.Timer(5, delayed_stop_recording).start()
-        
-        
-# Function to stop recording after delay if no motion is detected
-def delayed_stop_recording():
-    global recording
-    if recording and not motion_detected:
-        print('Stopping recording...')
-        camera.stop_recording()
-        recording = False
-        
-def reset_motion_status():
-    global motion_detected
-    motion_detected = False
-       
 camera = Picamera2()
+camera.global_camera_info()
 
-preview_config = camera.create_preview_configuration(main={"size": (1920, 1080)})
-
-vid_config = camera.create_video_configuration()
+vid_config = camera.create_video_configuration(main={"size": (1920, 1080)})
 camera.configure(vid_config)
 encoder = H264Encoder(10000000)
 
 pir = MotionSensor(14)
 
+def start_recording():
+    global recording, motion_detected, stop_recording_timer
+    if not recording:
+        print('Recording started.')
+        current_datetime = datetime.now()
+        formatted_date = current_datetime.strftime('%Y-%m-%d_%H-%M-%S')
+        video_filename = f'{video_directory}video_{formatted_date}.h264'
+        camera.start_recording(encoder, video_filename)
+        recording = True
+    motion_detected = True
+    #cancel pending stop recording action if motion is detected again
+    if stop_recording_timer is not None:
+        stop_recording_timer.cancel()
+        stop_recording_timer = None
+        print('Motion detected - Continuing recording.')
+
+def stop_recording():
+    global recording, motion_detected, stop_recording_timer
+    if recording:
+        motion_detected = False
+        #schedule stopping recording only if not already scheduled
+        if stop_recording_timer is None:
+            print('No motion detected - Stopping recording in 5 seconds...')
+            stop_recording_timer = threading.Timer(5, delayed_stop_recording)
+            stop_recording_timer.start()
+
+def delayed_stop_recording():
+    global recording, motion_detected, stop_recording_timer
+    #stop recording if no further motion was detected
+    if not motion_detected:
+        print('Stopping recording...')
+        camera.stop_recording()
+        recording = False
+    #reset the timer
+    stop_recording_timer = None
+
 pir.when_motion = start_recording
 pir.when_no_motion = stop_recording
-
-#thread to reset motion status after a delay
-def delayed_reset():
-    while True:
-        time.sleep(5) 
-        reset_motion_status()
-
-reset_thread = threading.Thread(target=delayed_reset)
-reset_thread.daemon = True
-reset_thread.start()
 
 pause()
